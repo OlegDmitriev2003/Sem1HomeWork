f79d8714cb0ba4c3ebd0e9b138dfbf7ac8e8cd93
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-21 14:35:26 +0300

    Finished Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int B = int.Parse(Console.ReadLine()!);
+
+if (A > B)
+    Console.Write($"{A} больше, чем {B} ");
+else if (A < B)
+    Console.Write($"{B} больше, чем {A}");
+else
+    Console.Write("Числа равны");
