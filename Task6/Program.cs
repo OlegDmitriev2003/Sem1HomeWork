8d1f400f23d7cba4e644c0769f9285a5e33e4880
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-21 15:07:33 +0300

    Finished Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+double Num = double.Parse(Console.ReadLine()!);
+if (Num % 2 == 0)
+    Console.WriteLine($"{Num} - чётное число");
+else
+    Console.WriteLine($"{Num} - нечётное число");
