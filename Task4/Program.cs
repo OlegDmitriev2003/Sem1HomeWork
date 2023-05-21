d165305efe41ab0ed312fdb3bb9f7e54d26202f8
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-21 14:53:54 +0300

    completed Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.Clear();
+int Max = 0;
+int A = 0;
+
+Console.Write("Введите первое число: ");
+ A = int.Parse(Console.ReadLine()!);
+if (Max < A )
+    Max = A;
+Console.Write("Введите второе число: ");
+ A = int.Parse(Console.ReadLine()!);
+if (Max < A )
+    Max = A;
+
+Console.Write("Введите третье число: ");
+ A = int.Parse(Console.ReadLine()!);
+if (Max < A )
+    Max = A;
+
+Console.WriteLine($"Число {Max} - наибольшее");
